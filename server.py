86bc25ff8399eccd50e7a4737365125946064b94
TrayHard <wwwtrayfrost@gmail.com>
2018-08-30 06:12:59 +0300

    Changed vk_api lib to vk

--- server.py
+++ server.py
@@ -1,15 +1,18 @@
-import vk_api
+import vk
 import data
 from flask import Flask, render_template
 app = Flask(__name__)
-vk = vk_api.VkApi(data.login, data.password)
-vk.auth()
+
+session = vk.AuthSession(data.appid, data.login, data.password)
+vk_api = vk.API(session, v='5.35', lang='ru', timeout=10)
 
 group_id = "-169050615"  # timenudge id
 
 
 @app.route('/')
 def index():
+    result = vk_api.wall.get(owner_id=group_id, count=10)
+    print(result)
     return render_template("index.html")
 
 
